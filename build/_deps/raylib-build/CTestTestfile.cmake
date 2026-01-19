392175d7230a03fb8350702ace07e3d350be8c13
JamesStanley <wiloe101@gmail.com>
2026-01-19 11:20:57 -0500

    I just changed the .gitignore to ignore the build/ directory also I deleted the build/ directory to clean up the repo

--- build/_deps/raylib-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/golde/source/repos/meow/build/_deps/raylib-src
-# Build directory: C:/Users/golde/source/repos/meow/build/_deps/raylib-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
